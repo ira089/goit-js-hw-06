d77e2500c684441b1ab204142dcc6ec8edd04cee
ira089 <gvir2015@gmail.com>
2023-10-20 23:24:07 +0300

    js6_1

---
+++ js/task01.js
@@ -0,0 +1,8 @@
+console.log("чаокакао");
+
+const ulCategories = document.querySelector("#categories");
+console.log(ulCategories);
+const childCategories = ulCategories.children;
+// const numberCategories = childCategories.length;
+// console.log(`Number of categories: ${numberCategories}`);
+console.log(`Number of categories: ${childCategories.length}`);
