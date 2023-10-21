cbfc70a97ddae3a1a474937c58e122ebddc29fb2
ira089 <gvir2015@gmail.com>
2023-10-21 23:53:54 +0300

    js6_a2

---
+++ js/task02.js
@@ -0,0 +1,21 @@
+console.log("чаокакао");
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const list = document.querySelector("#ingredients");
+// console.log(list);
+
+const markup = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+// console.log(markup);
+list.append(...markup);
