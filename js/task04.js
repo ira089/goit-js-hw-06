979cba445f826b50ed24716c9290fd9b2c17a7c3
ira089 <gvir2015@gmail.com>
2023-10-22 15:46:11 +0300

    js6_4b

--- js/task04.js
+++ js/task04.js
@@ -1,1 +1,18 @@
-console.log("чаокакао");
+const subBtn = document.querySelector('button[data-action="decrement"]');
+const addBtn = document.querySelector('button[data-action="increment"]');
+const spanValue = document.querySelector("#value");
+// console.log(spanValue);
+// console.dir(subBtn);
+// console.log(addBtn);
+let counterValue = 0;
+const hadlerSubBtnClick = () => {
+  counterValue -= 1;
+  spanValue.textContent = counterValue;
+};
+subBtn.addEventListener("click", hadlerSubBtnClick);
+
+const hadlerAddBtnClick = () => {
+  counterValue += 1;
+  spanValue.textContent = counterValue;
+};
+addBtn.addEventListener("click", hadlerAddBtnClick);
