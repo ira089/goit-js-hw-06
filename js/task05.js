044e1abd901cc48dbe63f4983118f52475eac626
ira089 <gvir2015@gmail.com>
2023-10-22 16:49:57 +0300

    js6_5b

--- js/task05.js
+++ js/task05.js
@@ -1,1 +1,18 @@
 console.log("чащкакащ");
+const inputName = document.querySelector("#name-input");
+const spanName = document.querySelector("#name-output");
+console.dir(inputName);
+console.log(spanName.textContent);
+
+const hadlerInputNameInput = (event) => {
+  event.preventDefault();
+  const valueInput = event.currentTarget.value;
+  console.dir(valueInput);
+  if (valueInput !== " ") {
+    spanName.textContent = valueInput;
+  } else {
+    spanName.textContent = "Anonymous";
+  }
+};
+
+inputName.addEventListener("input", hadlerInputNameInput);
